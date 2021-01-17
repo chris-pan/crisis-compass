53de2c5bc0673850a815f58a41f3154743268e4f
chris-pan <chris.pan719@gmail.com>
2021-01-16 18:28:24 -0800

    added skeleton

---
+++ nearest_icu.py
@@ -0,0 +1,10 @@
+table = #import table data
+user_location = #get user location
+
+#remove full hospitals
+#remove hospitals that are > x% full?
+state_table = #filter table to only hospitals in state
+k_closest = #get k closest locations
+#print k_closest hospital names + locations + % full
+
+#ask user to choose one and provide directions
