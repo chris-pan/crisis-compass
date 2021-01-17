97354ad27cdde7da28165a033e067c4c78322a7e
Rishabh Chittaranjan <rishabhc@live.com>
2021-01-17 00:48:18 -0800

    Update Update_Spreadsheet.py

--- Update_Spreadsheet.py
+++ Update_Spreadsheet.py
@@ -1,42 +1,34 @@
-import urllib   #to get the spreadsheet
-import datetime #to get the calendar date
-import os
-import requestsdef exists(path):
-    r = requests.head(path)
-    return r.status_code == requests.codes['ok']
+import pandas as pd
+import datetime
+import numpy as np
 
-#first define path of file
-#define the location
-location="\\something\\something"
-#define the file name
-file_title="data.csv"
-path=location.join(file_title)
-#download the current file
-dls="https://healthdata.gov/sites/default/files/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries_20210110.csv"
+## just kept a separate filtering method though it could be kept in the same line, change if want
+def filter_by_capacity(icu_percentage, inpatient_percentage, data):
+    return data[((data["percentage inpatient"]<inpatient_percentage) & (data["percentage inpatient"]>=0.0))|((data["percentage icu"]<icu_percentage) & (data["percentage icu"]>=0.0))]
 
-urllib.request.urlretrieve(dls,path)#downloads the file and saves to path
-
-current_date=datetime.date
-next_week=current_date+datetime.timedelta(days=7)
-string_date=next_week.isoformat
-print(string_date.replace('-',''))
-next_dls="https://healthdata.gov/sites/default/files/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries_"+string_date+".csv" #next file to download
-
-while(True):#has to run forever to check if the file exists
-    #constantly update the time
-    #if file exists- a week has passed already
-    if(exists(next_dls)):
-        #first delete current file
-        os.remove(path)
-        #download new file
-        urllib.request.urlretrieve(next_dls,path)
-        #update the next_dls link
-        current_date=next_week
-        next_week=current_date+datetime.timedelta(days=7)
-        string_date=next_week.isoformat
-        print(string_date.replace('-',''))
-        next_dls="https://healthdata.gov/sites/default/files/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries_"+string_date+".csv"
-
-
+#first filters by state, calculates and sorts the df by distance, then if you want, can filter by threshold icu inpatient
+def filter_by_state_calc_distance(current_location, icu_percentage, inpatient_percentage, data):
+    #assuming that will get an address
+    my_state=current_location[-2:]#grabbing the state from the last two letters of the address- make sure no trailing spaces in str
+    state_df=data.query('state==@my_state') # can add filter for percentage in icu or inpatient
+    ##################################PUT UR GOOGLE DISTANCE STUFF HERE
+    #for row in len(state_df):
+    #    state_df["distance"][row]=distance(current_location,state_df['address'][row])
+    #state_df.sort_values(by=["distance"])--sort dataframe by distance in ascending order-> can pick the k closest by then
+    return filter_by_capacity(icu_percentage,inpatient_percentage,state_df)
     
+
+
+
+hospital_data=pd.read_csv("https://healthdata.gov/sites/default/files/reported_hospital_capacity_admissions_facility_level_weekly_average_timeseries_20210110.csv") # get the file
+#taking specific columns from data
+bed_data=pd.concat([hospital_data["state"],hospital_data["hospital_name"],hospital_data["address"],hospital_data["city"],hospital_data["zip"],hospital_data["inpatient_beds_used_7_day_sum"],hospital_data["inpatient_beds_7_day_sum"],hospital_data["icu_beds_used_7_day_sum"],hospital_data["total_icu_beds_7_day_sum"]],axis=1,keys=["state","hospital_name","address","city","zip","inpatient_beds_used_7_day_sum","inpatient_beds_7_day_sum","icu_beds_used_7_day_sum","total_icu_beds_7_day_sum"])
+#calculating the percentage of inpatient and icu beds being used
+bed_data["percentage inpatient"]=np.where(((bed_data["inpatient_beds_7_day_sum"]<=0) & (bed_data["inpatient_beds_used_7_day_sum"]<0)),np.NaN,bed_data["inpatient_beds_used_7_day_sum"]/bed_data["inpatient_beds_7_day_sum"])
+bed_data["percentage icu"]=np.where(((bed_data["total_icu_beds_7_day_sum"]<=0) & (bed_data["icu_beds_used_7_day_sum"]<0)),np.NaN,bed_data["icu_beds_used_7_day_sum"]/bed_data["total_icu_beds_7_day_sum"])
+#column for the distance to be calculated by Google API
+bed_data["distance"]=np.NaN
+
+filtered_df=filter_by_state_calc_distance("CA",0.50,0.50,bed_data)
+display(filtered_df)
 
